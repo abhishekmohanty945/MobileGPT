91e6570e94832737fe038eb5b8345b3d6d843e34
abhishek <abhishekmohanty945@gmail.com>
2023-10-22 19:59:51 -0700

    remove api key

--- app/src/main/java/com/example/assignment3_277/constants/Constants.kt
+++ app/src/main/java/com/example/assignment3_277/constants/Constants.kt
@@ -1,5 +1,3 @@
 package com.example.assignment3_277.constants
 
-const val openAIApiKey = "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"
-
-//const val openAIApiKey = "sk"
+const val openAIApiKey = "YOUR_KEY_HERE"
